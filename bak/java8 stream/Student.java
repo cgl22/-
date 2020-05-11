cf327c2d88f4377398b4b3f581db83f8ad53d723
陈贵龙 <chenguilong@infisa.cn>
2020-05-11 11:18:10 +0800

    stream介绍

---
+++ bak/java8 stream/Student.java
@@ -0,0 +1,73 @@
+package cn.infisa.inquiry;
+
+public class Student {
+	private String name;
+	private int age;
+	private String num;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((num == null) ? 0 : num.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (num == null) {
+			if (other.num != null)
+				return false;
+		} else if (!num.equals(other.num))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "[name=" + name + ", age=" + age + ", num=" + num + "]";
+	}
+
+	public Student(String name, int age, String num) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.num = num;
+	}
+
+	public Student() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getNum() {
+		return num;
+	}
+
+	public void setNum(String num) {
+		this.num = num;
+	}
+
+}
